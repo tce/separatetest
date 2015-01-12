059701400ce7ae7625656cd5cdd85218c704da67
teskridge <teskridge@fit.edu>
2015-01-12 12:00:21 -0500

    another comment

--- src/edu/fit/icis/ICISMain.java
+++ src/edu/fit/icis/ICISMain.java
@@ -3,6 +3,7 @@ package edu.fit.icis;
 /**
  * Changed this in the APL branch.
  * We'll want to do lots of stuff on this branch
+ * lots of stuff
  * Created by teskridge on 1/12/15.
  */
 public class ICISMain
