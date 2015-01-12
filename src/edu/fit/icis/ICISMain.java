62e04543b210eddda760f09752ae5d52964dc8bd
teskridge <teskridge@fit.edu>
2015-01-12 12:44:40 -0500

    updated from aplgold branch

--- src/edu/fit/icis/ICISMain.java
+++ src/edu/fit/icis/ICISMain.java
@@ -4,8 +4,14 @@ package edu.fit.icis;
  * Changed this in the APL branch.
  * We'll want to do lots of stuff on this branch
  * lots of stuff
+ * 
+ * change on apl
  * Created by teskridge on 1/12/15.
  */
 public class ICISMain
 {
+    public static void main(String[] args)
+    {
+        System.out.println("Calling ICISMain");
+    }
 }
