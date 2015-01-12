121b6d57b39adb9795b7946abe5cf921ef740af2
teskridge <teskridge@fit.edu>
2015-01-12 12:23:22 -0500

    added "change on apl"

--- src/edu/fit/icis/ICISMain.java
+++ src/edu/fit/icis/ICISMain.java
@@ -4,6 +4,8 @@ package edu.fit.icis;
  * Changed this in the APL branch.
  * We'll want to do lots of stuff on this branch
  * lots of stuff
+ * 
+ * change on apl
  * Created by teskridge on 1/12/15.
  */
 public class ICISMain
