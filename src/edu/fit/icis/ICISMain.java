afc97f87aa5315a2fc585f48fac17b128fcb2c3d
teskridge <!@faith>
2015-01-12 14:40:51 -0500

    added a method

--- src/edu/fit/icis/ICISMain.java
+++ src/edu/fit/icis/ICISMain.java
@@ -1,11 +1,11 @@
 package edu.fit.icis;
 
 /**
- * Changed this in the APL branch.
- * We'll want to do lots of stuff on this branch
- * lots of stuff
+ * We are on the FIT branch
+ * APL branches should not mention FIT
  * 
- * change on apl
+ * 
+ *
  * Created by teskridge on 1/12/15.
  */
 public class ICISMain
